03101c6fdf63f43ea155e7911499852433ea25e2
Badra <badra10alou10@gmail.com>
2025-07-18 23:46:46 +0000

    template update

--- boutique/views.py
+++ boutique/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """Vue pour la page d'accueil de la boutique"""
+    return render(request, 'boutique/home.html', {
+        'title': 'Accueil Boutique'
+    })
